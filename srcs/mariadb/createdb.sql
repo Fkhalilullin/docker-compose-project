8ab75bd6b312524ea7a22e986b0465852095c97b
Makeda Teressa <mteressa@pl-g1.msk.21-school.ru>
2021-11-27 14:58:20 +0300

    doesnt work :(

--- srcs/mariadb/createdb.sql
+++ srcs/mariadb/createdb.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE mteressa;
-CREATE USER 'mte'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON mteressa.* TO 'mte'@'%';
+CREATE DATABASE Mteressa;
+CREATE USER 'mteressa'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON Mteressa.* TO 'mteressa'@'%';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
