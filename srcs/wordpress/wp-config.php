317b8eea741adf876c5b48324f9a7a7df1e827de
Makeda Teressa <mteressa@pl-g1.msk.21-school.ru>
2021-11-27 16:33:42 +0300

    check

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'DVSGroupDB' );
+define( 'DB_NAME', 'Mteressa' );
 
 /** MySQL database username */
-define( 'DB_USER', 'dvs' );
+define( 'DB_USER', 'mte' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'password' );
